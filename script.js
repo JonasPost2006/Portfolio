389b631e8fb9b1a4dbbecef8b7073257e0e19cd8
JonasPost2006 <43406465+JonasPost2006@users.noreply.github.com>
2024-03-19 22:23:47 +0100

    Hele mooie website prachtig hoor

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let currentImageIndex = 0;
-    const images = ["Fusion.png", "Rubik's-solver.png", "Rubik's-solver.jpeg"]; // Add more image URLs as needed
-    const slideshowInterval = 5000; // 5 seconds
-
-    function showNextImage() {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        const imageSrc = images[currentImageIndex];
-        document.querySelector(".slideshow-img").src = imageSrc;
-    }
-
-    const slideshowTimer = setInterval(showNextImage, slideshowInterval);
-
-    document.getElementById("image").addEventListener("click", function () {
-        clearInterval(slideshowTimer); // Stop the slideshow when clicked
-        // Add any additional behavior you want when the image is clicked
+document.querySelector('nav a[href="#projects"]').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent default link behavior
+    document.querySelector('#projects').scrollIntoView({ // Scroll smoothly to the projects section
+        behavior: 'smooth'
     });
 });
